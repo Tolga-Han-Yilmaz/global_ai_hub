5f0cabe3430515685524eafca5cfe761109545c1
Tolga <06thy40@gmail.com>
2022-08-14 20:25:26 +0300

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,20 @@
 import navStyle from "./navbar.module.css";
 import logo from "../../assets/globalaihub.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
       <div className={navStyle["container"]}>
         <img src={logo} alt="" />
-        <a
-          href="https://twitter.com/Dave_Conner"
-          className={navStyle["btn btn-2"]}
-        >
-          Hover
-        </a>
+        <div className={navStyle["course"]}>
+          <Link to="/" className={navStyle["btn"]}>
+            My Courses
+          </Link>
+          <Link to="/" className={navStyle["btn"]}>
+            courses
+          </Link>
+        </div>
       </div>
     </nav>
   );
